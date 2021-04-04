2846979a9e16389b4518016526a79854eba641f3
ihniwiad <mail@matthiasbroecker.de>
2021-04-04 18:48:44 +0200

    UPDATE: lazy img

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -5,9 +5,28 @@
     <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
     <div class="p">
-        <?php if ( has_post_thumbnail() ) {
-            the_post_thumbnail( 'large', [ 'class' => 'img-fluid' ] );
-        } ?>
+        <?php
+            // load lazy
+            $attachment_id = get_post_thumbnail_id( $post ); 
+            $alt = get_post_meta( $attachment_id, '_wp_attachment_image_alt', true );
+            $image_attributes = wp_get_attachment_image_src( $attachment_id, 'large' ); // returns array( $url, $width, $height )
+
+            if ( $image_attributes ) {
+                $img_data = array(
+                  'img' => array(
+                    'url' => $image_attributes[ 0 ],
+                    'width' => $image_attributes[ 1 ],
+                    'height' => $image_attributes[ 2 ],
+                    'alt' => $alt
+                  )
+                );
+
+                if ( class_exists( 'LazyImg' ) && method_exists( LazyImg, 'print' ) ) {
+                    $thumbnail = new LazyImg( $img_data );
+                    $thumbnail->print();
+                }
+            }
+        ?>
     </div>
 
     <?php the_content(); ?>
